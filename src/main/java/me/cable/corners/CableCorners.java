20bb43822ccd0c7a1bc85de014ec8e23b172c9f0
Cable <cableplays1912@gmail.com>
2022-02-26 17:06:31 +0200

    Initial commit

---
+++ src/main/java/me/cable/corners/CableCorners.java
@@ -0,0 +1,12 @@
+package me.cable.corners;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CableCorners extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
